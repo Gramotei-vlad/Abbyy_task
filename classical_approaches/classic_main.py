7ea104fa95f8c6ae9e9e81534a4fd7a2acddcb8a
v.kosukhin <v.kosukhin@tinkoff.ru>
2021-12-19 23:49:23 +0300

    Добавил классический подход.

--- classical_approaches/classic_main.py
+++ classical_approaches/classic_main.py
@@ -1,20 +1,47 @@
 import argparse
 
 from datasets import load_dataset
-from dataset import get_dataset
+from dataset import get_dataset, get_class_map
 from viterbi_algorithm import ViterbiAlgorithm
+from seqeval.metrics import classification_report
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser('Запуск классического подхода для задачи POS')
-    parser.add_argument('--input', type=str, default='She saw a cat.',
+    parser.add_argument('--input', type=str, default='She saw a cat',
                         help='Строка, которую нужно разметить')
+    parser.add_argument('--predict', action='store_true', default=False,
+                        help='Если истина, то выдаст результат для строки в args.input')
     args = parser.parse_args()
 
     dataset = load_dataset('conll2003')
     train_texts, train_labels = get_dataset(dataset, 'train')
     test_texts, test_labels = get_dataset(dataset, 'test')
 
+    label2idx = get_class_map()
+    idx2label = {idx: label for label, idx in label2idx.items()}
+
     algorithm = ViterbiAlgorithm()
-    algorithm.tag(train_texts, train_labels)
+    all_tags = algorithm.tag(train_texts, train_labels)
+
+    result_predictions = []
+    result_labels = []
+    if args.predict:
+        split_sentence = args.input.split()
+        prediction = algorithm.run(split_sentence, all_tags)
+
+        for word, pred_class in zip(split_sentence, prediction):
+            tag = idx2label[pred_class]
+            print(f'Word: {word}, prediction_class: {tag}')
+    else:
+        for text, labels in zip(test_texts, test_labels):
+            prediction = algorithm.run(text, all_tags)
+            tags_prediction = [idx2label[pred] for pred in prediction]
+            tags_labels = [idx2label[label] for label in labels]
+            result_predictions.append(tags_prediction)
+            result_labels.append(tags_labels)
+
+        print(classification_report(result_labels, result_predictions))
+
+
--- classical_approaches/viterbi_algorithm.py
+++ classical_approaches/viterbi_algorithm.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, Counter
+from typing import List, Tuple, Union
 
 
 START_PROB = 1e-10
@@ -6,80 +7,81 @@ START_PROB = 1e-10
 
 class ViterbiAlgorithm:
     def __init__(self):
-        self.unigram_cnt = defaultdict(lambda x: 0)
-        self.bigram_cnt = defaultdict(lambda x: 0)
-        self.tag_cnt = defaultdict(lambda x: 0)
+        self.unigram_cnt = defaultdict(lambda: 0)
+        self.bigram_cnt = defaultdict(lambda: 0)
+        self.tag_cnt = defaultdict(lambda: 0)
         self.tag_word_cnt = Counter()
         self.transition_probs = defaultdict(lambda: START_PROB)
         self.emmission_probs = defaultdict(lambda: START_PROB)
 
     @staticmethod
-    def get_ngrams(text, n):
+    def get_ngrams(text: List[str], n: int):
         return [tuple(text[i: i + n]) for i in range(len(text))]
 
-    def get_bigram_cnt(self, tags):
+    def get_bigram_cnt(self, tags: List[Union[str, int]]):
         ngrams = self.get_ngrams(tags, 2)
         for bigram in ngrams:
             self.bigram_cnt[bigram] += 1
         return self.bigram_cnt
 
-    def get_unigram_cnt(self, tags):
+    def get_unigram_cnt(self, tags: List[Union[str, int]]):
         for tag in tags:
             self.unigram_cnt[tag] += 1
         return self.unigram_cnt
 
-    def get_tag_and_words_cnt(self, tags_and_words):
+    def get_tag_and_words_cnt(self, tags_and_words: List[Tuple[int, str]]):
         for tag, word in tags_and_words:
             self.tag_cnt[tag] += 1
             self.tag_word_cnt[(tag, word)] += 1
         return self.tag_word_cnt
 
-    def get_transition_probs(self, tags):
+    def get_transition_probs(self, tags: List[Union[str, int]]):
         bigrams = self.get_ngrams(tags, 2)
         for bigram in bigrams:
             self.transition_probs[bigram] = self.bigram_cnt[bigram] / self.unigram_cnt[bigram[0]]
         return self.transition_probs
 
-    def get_emmission_probs(self, tags_and_words):
+    def get_emmission_probs(self, tags_and_words: List[Tuple[int, str]]):
         for tag, word in tags_and_words:
             self.emmission_probs[(tag, word)] = self.tag_word_cnt[(tag, word)] / self.tag_cnt[tag]
         return self.emmission_probs
 
-    def initial_probabilities(self, tag):
+    def initial_probabilities(self, tag: int):
         return self.transition_probs["START", tag]
 
-    def run(self, observable, in_states):
+    def run(self, input_text: List[str], in_states: List[Union[int, str]]):
         states = set(in_states)
         states.remove("START")
         states.remove("END")
         trails = {}
         for s in states:
-            trails[s, 0] = self.initial_probabilities(s) * self.emmission_probs[s, observable[0]]
+            trails[s, 0] = self.initial_probabilities(s) * self.emmission_probs[s, input_text[0]]
 
-        for o in range(1, len(observable)):
-            obs = observable[o]
+        for o in range(1, len(input_text)):
+            obs = input_text[o]
             for s in states:
                 v1 = [(trails[k, o - 1] * self.transition_probs[k, s] * self.emmission_probs[s, obs], k)
                       for k in states]
                 k = sorted(v1)[-1][1]
                 trails[s, o] = trails[k, o - 1] * self.transition_probs[k, s] * self.emmission_probs[s, obs]
-        best_path = []
-        for o in range(len(observable) - 1, -1, -1):
-            k = sorted([(trails[k, o], k) for k in states])[-1][1]
-            best_path.append((observable[o], k))
-        best_path.reverse()
-        for x in best_path:
-            print(str(x[0]) + "," + str(x[1]))
-        return best_path
 
-    def tag(self, sentences, tags):
+        result_answer = []
+        for o in range(len(input_text) - 1, -1, -1):
+            k = sorted([(trails[k, o], k) for k in states])[-1][1]
+            result_answer.append(k)
+        result_answer.reverse()
+        return result_answer
+
+    def tag(self, sentences: List[List[str]], tags: List[List[int]]):
         tagged_words = []
         all_tags = []
         for sentence, tag in zip(sentences, tags):
             all_tags.append("START")
+
+            assert len(sentence) == len(tag)
             for (word, label) in zip(sentence, tag):
-                all_tags.append(tag)
-                tagged_words.append((tag, word))
+                all_tags.append(label)
+                tagged_words.append((label, word))
             all_tags.append("END")
 
         self.get_tag_and_words_cnt(tagged_words)
@@ -89,4 +91,6 @@ class ViterbiAlgorithm:
 
         self.get_transition_probs(all_tags)
         self.get_emmission_probs(tagged_words)
+
+        return all_tags
 
